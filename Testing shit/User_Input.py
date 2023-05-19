1be47134b0ed0bfd537f127d4cd44cb5c6708a42
KaiserHua1901 <KaiserHua1901@outlook.com>
2023-05-19 13:40:18 -0700

    Revert "Revert "For testing purpose""
    This reverts commit 8f39994ffb4140aad125e667bdb443398396e4b6.

---
+++ Testing shit/User_Input.py
@@ -0,0 +1,41 @@
+#age = 21
+#print(f"my age is {age} this year" )
+
+# adjective1 = input("Enter an adjective: ")
+# noun = input("Enter a noun: ")
+# adjective2 = input("Enter an adjective: ")
+# verb = input("Enter a verb: ")
+# adjective3 = input("Enter an adjective: ")
+
+# print(f"Today I went to a {adjective1} zoo.")
+# print(f"In an exhibit, I saw {noun}.")
+# print(f"{noun} was {adjective2} and {verb}ing.")
+# print(f"I was {adjective3}!")
+
+#This can be use to caculated area of rectangle
+
+# length = float(input("Enter the length of rectangle: "))
+# width = float(input("Enter the width of Rectangle: "))
+
+# Result = length * width 
+# print (F"The area of rectangle is {Result} cm^2")
+
+#This is to caculated how much the total cost of a item and amount of item you like to buy
+
+# Items = input("What item you like to have:")
+# Price = float(input("How much is the price: "))
+# Quantity = int(input("how many would you buy: "))
+
+# Total = Quantity * Price
+
+# print ("the item you like is " + Items)
+# print ("The total amount of is " +str(round(Total,2)) + " dollar")
+
+# #other way to write the above code
+# print (f"The total amount of is  {round(Total,2)} dollar")
+
+print (round(21.23892312, 2))
+
+print ("for testing purpose")
+
+
