110646ba7033bc4ef45d6880fe97e01550b28728
KaiserHua1901 <KaiserHua1901@outlook.com>
2023-05-20 13:53:27 -0700

    Nothing

---
+++ Temp Converter.py
@@ -0,0 +1,13 @@
+unit = input("Please enter temp unit of C or F: ")
+#temp = float(input("Please Enter the temperature: "))
+
+
+if unit == "C":
+    print("unit is C")
+elif unit == "F":
+    print("unit is F")
+else:
+    print("Not check")
+
+
+    
