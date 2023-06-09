4c1c7eb26b1628c2b3450ce440c8dc603a670a59
KaiserHua1901 <KaiserHua1901@outlook.com>
2023-06-09 16:20:57 -0700

    function for check numb is finish

--- Mini_Projects/Rock_Paper_Scissors.py
+++ Mini_Projects/Rock_Paper_Scissors.py
@@ -1,6 +1,31 @@
 import random
 
-guess = ['rock', 'paper' , 'Scissors']
+def Check_if_input_is_int(numb):
+    while True:
+        if numb.isdigit():
+            if int(numb) <= 3 and int(numb) != 0:
+                numb = int(numb)
+                break
+        else: 
+            print('Please type in number & is smaller than 4 next time.')
+         
+        return(numb)
 
-print(random.choice(guess))
+choice = ['rock', 'paper' , 'Scissors']
+pc_guess = random.choice(choice)
+
+print(pc_guess)
+
+while True:
+    player_choice = input("Please choice 1 for rock, 2 for paper, 3 for scissors: ")
+    
+    Check_if_input_is_int(player_choice)
+    continue
+
+
+
+
+
+
+
 
