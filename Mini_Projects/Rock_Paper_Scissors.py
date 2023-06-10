97e8b88185602d64bf75ce9a04897a48fb1dbc52
KaiserHua1901 <KaiserHua1901@outlook.com>
2023-06-09 17:23:48 -0700

    Mini Project finish rock,paper,scissors

--- Mini_Projects/Rock_Paper_Scissors.py
+++ Mini_Projects/Rock_Paper_Scissors.py
@@ -1,26 +1,44 @@
 import random
+from Function_project import Check_if_input_is_int
 
-def Check_if_input_is_int(numb):
-    while True:
-        if numb.isdigit():
-            if int(numb) <= 3 and int(numb) != 0:
-                numb = int(numb)
-                break
-        else: 
-            print('Please type in number & is smaller than 4 next time.')
-         
-        return(numb)
+choice = [1, 2 , 3]
 
-choice = ['rock', 'paper' , 'Scissors']
-pc_guess = random.choice(choice)
 
-print(pc_guess)
+
+score = 0
 
 while True:
-    player_choice = input("Please choice 1 for rock, 2 for paper, 3 for scissors: ")
+    pl_choice = Check_if_input_is_int()
+    pc_guess = random.choice(choice)
+    print(pc_guess)
+    if pl_choice == "q" or pl_choice == "Q":
+        break
+    else:
+        if pl_choice == pc_guess:
+            print('This is a draw')
+        elif pl_choice == 1 and pc_guess == 2:
+            print("You Lose")
+            score -= 1
+        elif pl_choice == 1 and pc_guess == 3:
+            print('You Win')
+            score += 1
+        elif pl_choice == 2 and pc_guess == 1:
+            print('You win')
+            score += 1
+        elif pl_choice == 2 and pc_guess == 3:
+            print('You lose')
+            score -= 1
+        elif pl_choice == 3 and pc_guess == 2:
+            print('You win')
+            score += 1
+        elif pl_choice == 3 and pc_guess == 1:
+            print('You Lose')
+            score -= 1
+
+print(f'Your score is {score}')
     
-    Check_if_input_is_int(player_choice)
-    continue
+
+
 
 
 
